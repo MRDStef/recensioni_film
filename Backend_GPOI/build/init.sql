52a6b9e6caf495ba9b3c0351eb3cc2792a2801c5
MRDStef <stefanoandrade9@gmail.com>
2026-05-29 23:42:53 +0200

    init sql completato

--- Backend_GPOI/build/init.sql
+++ Backend_GPOI/build/init.sql
@@ -6,14 +6,19 @@ CREATE TABLE IF NOT EXISTS Film (
     titolo VARCHAR(255) NOT NULL UNIQUE,
     genere VARCHAR(255) NOT NULL,
     regista VARCHAR(255),
-    data_pubblicazione DATE NOT NULL
+    data_pubblicazione DATE NOT NULL,
+    locandina_url VARCHAR(255),
+    descrizione TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Account (
     id_account BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome_utente VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    ruolo ENUM("moderatore", "utente") NOT NULL
+    ruolo ENUM('utente', 'admin') NOT NULL DEFAULT 'utente',
+    password VARCHAR(255) NOT NULL,
+    avatar_url VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS Recensione (
@@ -21,7 +26,8 @@ CREATE TABLE IF NOT EXISTS Recensione (
     id_film BIGINT NOT NULL,
     id_account BIGINT NOT NULL,
     valutazione INT NOT NULL CHECK (valutazione >= 1 AND valutazione <= 5),
-    descrizione VARCHAR(255),
+    descrizione descrizione TEXT(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (id_film) REFERENCES Film(id_film),
     FOREIGN KEY (id_account) REFERENCES Account(id_account)
